3b174017780d2c99255de11edc79d6ced970a381
Jevin Sweval <jevinsweval@gmail.com>
2024-03-09 16:13:29 -0500

    find-by-magic.py: add -m to match by MIME type

--- find-by-magic.py
+++ find-by-magic.py
@@ -8,9 +8,10 @@ from pathlib import Path
 import magic
 
 m = magic.Magic(raw=True)
+mmime = magic.Magic(raw=True, mime=True)
 
 
-def PathDir(string):
+def PathDir(string) -> Path:
     path = Path(string)
     if not path.is_dir():
         msg = f"{path} is not a directory"
@@ -18,8 +19,11 @@ def PathDir(string):
     return path
 
 
-def is_match(args, pth):
-    ftype = m.from_buffer(open(pth, "rb").read(4096))
+def is_match(args, pth) -> bool:
+    if args.regexes is not None or args.ftypes is not None:
+        ftype = m.from_file(pth)
+    if args.mtypes is not None:
+        mtype = mmime.from_file(pth)
     if args.regexes is not None:
         for pat in args.regexes:
             if re.search(pat, ftype) is not None:
@@ -27,10 +31,13 @@ def is_match(args, pth):
     if args.ftypes is not None:
         if ftype in args.ftypes:
             return True
+    if args.mtypes is not None:
+        if mtype in args.mtypes:
+            return True
     return False
 
 
-def main(args):
+def main(args) -> int:
     for pdir in args.dirs:
         for pth in pdir.glob("**/*"):
             if not pth.is_file():
@@ -38,7 +45,7 @@ def main(args):
             if args.exts is not None and pth.suffix not in args.exts:
                 continue
             if is_match(args, pth):
-                print(str(pth))
+                print(pth)
     return 0
 
 
@@ -53,6 +60,15 @@ if __name__ == "__main__":
         type=str,
         action="append",
         help="File type to find",
+    )
+    parser.add_argument(
+        "-m",
+        "--mime",
+        metavar="MIME_TYPE",
+        dest="mtypes",
+        type=str,
+        action="append",
+        help="MIME type to find",
     )
     parser.add_argument(
         "-r",

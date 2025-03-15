f61a7b015e8c2da0ea0df317defffb0012301c68
Jevin Sweval <jevinsweval@gmail.com>
2025-03-15 13:21:45 -0400

    xnu-dump-image-activation-stack improvements

--- xnu-dump-image-activation-stack.c
+++ xnu-dump-image-activation-stack.c
@@ -1,10 +1,13 @@
 #include <pthread.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/param.h>
+#include <sys/types.h>
 
 int main(int argc, const char **argv, const char **envp, const char **apple) {
     size_t i;
+    bool got_null;
 
     void *stackaddr  = pthread_get_stackaddr_np(pthread_self());
     size_t stacksize = pthread_get_stacksize_np(pthread_self());
@@ -15,38 +18,52 @@ int main(int argc, const char **argv, const char **envp, const char **apple) {
     printf("frameaddr: %p\n", frameaddr);
 
     printf("argc:  %d\n", argc);
-    printf("argv:  %p\n", (void *)argv);
-    for (i = 0; i < (size_t)argc + 1; ++i) {
-        printf("argv[%zu]  = '%s'\n", i, argv[i]);
-        printf("&argv[%zu] = %p\n", i, (void *)&argv[i]);
-    }
 
-    const char **tenvp   = envp;
-    const char **tapplep = apple;
+    printf("argv: %p\n", (void *)argv);
+    i                  = 0;
+    got_null           = false;
+    const char **targp = argv;
+    do {
+        const char *targ = *targp;
+        printf("argv[%zu]  = %p '%s'\n", i, (void *)targ, targ);
+        printf("&argv[%zu] = %p\n", i, (void *)targp);
+        ++i;
+        ++targp;
+        got_null = !targ;
+    } while (!got_null);
 
     printf("envp: %p\n", (void *)envp);
-    i = 0;
+    i                  = 0;
+    got_null           = false;
+    const char **tenvp = envp;
     do {
         const char *tenv = *tenvp;
         printf("envp[%zu]  = %p '%s'\n", i, (void *)tenv, tenv);
         printf("&envp[%zu] = %p\n", i, (void *)tenvp);
         ++i;
         ++tenvp;
-    } while (*tenvp && tenvp != tapplep);
+        got_null = !tenv;
+    } while (!got_null);
 
     printf("apple: %p\n", (void *)apple);
-    i = 0;
+    i                    = 0;
+    got_null             = false;
+    const char **tapplep = apple;
     do {
         const char *tapple = *tapplep;
         printf("apple[%zu]  = %p '%s'\n", i, (void *)tapple, tapple);
         printf("&apple[%zu] = %p\n", i, (void *)tapplep);
         ++i;
         ++tapplep;
-    } while (*tapplep && tapplep != stackaddr);
+        got_null = !tapple;
+    } while (!got_null);
 
-    const char *strp = (const char *)roundup((uintptr_t)tapplep, 16);
-    i                = 0;
-    size_t slen      = 0;
+    const char *strp            = (const char *)roundup((uintptr_t)tapplep, 16);
+    uintptr_t apple_str_padding = (uintptr_t)strp - (uintptr_t)tapplep;
+    printf("padding between apple array pointers and string area: %zu\n", apple_str_padding);
+    printf("string area: %p\n", (void *)strp);
+    i           = 0;
+    size_t slen = 0;
     do {
         slen = strlen(strp);
         printf("%p: len: %zu '%s'\n", (void *)strp, slen, strp);

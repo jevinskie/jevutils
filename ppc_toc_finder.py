cf7e27b7c6a489e8c393e9e873b5e495e92b8463
Jevin Sweval <jevinsweval@gmail.com>
2025-07-29 22:49:54 -0400

    works (sample N = 1)

--- ppc_toc_finder.py
+++ ppc_toc_finder.py
@@ -19,22 +19,25 @@ def find_potential_tocs_raw(bin_buf: bytes, base: int) -> list[int]:
 
     raw_len = len(bin_buf)
     quad_off = raw_len % 8
-    print(f"raw_len: {raw_len} % 8: {quad_off}")
-    if raw_len:
+    if (raw_len == 0) or (quad_off != 0):
         bin_buf = bin_buf + bytes(8 - quad_off)
-
     # bswap_bin = bytearray(bin_buf)
     # for i in range(0, raw_len, 8):
     #     bswap_bin[i : i + 8] = reversed(bswap_bin[i : i + 8])
 
     arr = array.array("Q", bin_buf)
-    # arr.byteswap()
+    arr.byteswap()
     alen = len(arr)
-    print(f"len(arr): {alen}")
+
+    first8_int = arr[0]
+    print(
+        f"base: {base:#018x} alen: {alen:6} first8: 0x{bin_buf[:8].hex()} first8_int: {first8_int:#018x} raw_len: {raw_len} % 8: {quad_off}"
+    )
+    if first8_int == base + 0x8000:
+        maybe_tocs.append(base)
 
     for i in range(alen - (0x8000 // 8)):
-        this_addr = i * 8
-        this_addr += base
+        this_addr = base + (i * 8)
         mb_toc_addr = this_addr + 0x8000
         this_value = arr[i]
         # print(f"this_addr: {this_addr:#x}")
@@ -63,6 +66,8 @@ def find_potential_tocs(bin_path: Path, base: int | None = None) -> list[int]:
             print(f"i: {i} seg: {seg}")
             seg_va = seg.virtual_address
             seg_buf = bytes(seg.content)
+            if seg.name == ".got":
+                open("got-dump.bin", "wb").write(seg_buf)
             maybe_tocs += find_potential_tocs_raw(seg_buf, seg_va)
 
     return maybe_tocs

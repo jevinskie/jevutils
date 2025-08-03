f3566361d4568ca36a95dce9414cfdcda4421ec6
Jevin Sweval <jevinsweval@gmail.com>
2025-08-03 15:00:35 -0400

    better wip

--- acrm.py
+++ acrm.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
+from __future__ import annotations
 
 import argparse
+from typing import Self
 
-import anytree
+from anytree import NodeMixin, RenderTree
 from attrs import define, field
 from path import Path
-from rich import print
+from rich import print as rprint
 from rich.pretty import pprint
+from rich.traceback import install as rtinstall
+
+rtinstall(show_locals=True)
 
 
 def is_cached_file(p: Path) -> bool:
@@ -15,33 +20,83 @@ def is_cached_file(p: Path) -> bool:
     return p.name == "config.cache"
 
 
-@define
-class CachedDir(anytree.NodeMixin):
+@define(init=False, slots=False)
+class PTree(NodeMixin):
     fspath: Path = field()
+    cache_path: Path | None = field(default=None)
 
-    @fspath.validator  # type: ignore
-    def _check_p(self, attr, p: Path):
-        if not p.is_file():
-            raise TypeError(f"CachedDir fspath '{p}' is not a file")
+    def __init__(self, path: Path, parent: PTree | None, cache_path: Path | None = None) -> None:
+        super().__init__()
+        if cache_path is not None:
+            if not cache_path.is_file():
+                raise TypeError(f"CachedDir cache_path '{cache_path}' is not a file")
+        self.fspath = path
+        self.parent = parent
+        self.cache_path = cache_path
 
+    @property
+    def cached(self) -> bool:
+        return self.cache_path is not None
+
+    @property
     def parts(self):
         return self.fspath.parts()
 
+    @classmethod
+    def from_cache_file(cls, cache_file: Path, root: Path) -> Self:
+        if not cache_file.is_file():
+            raise TypeError(f"CachedDir cache_file '{cache_file}' is not a file")
+        dir_path = cache_file.parent
+        dpp = dir_path.parent
+        is_root = dir_path.samefile(root)
+        return cls(
+            dir_path,
+            parent=cls.from_dir(dpp, root) if not is_root else None,
+            cache_path=cache_file,
+        )
 
-def get_cached_dirs(root: Path) -> list[CachedDir]:
+    @classmethod
+    def from_dir(cls, dir_path: Path, root: Path) -> Self:
+        if not dir_path.is_dir():
+            raise TypeError(f"CachedDir dir_path '{dir_path}' is not a directory")
+        dpp = dir_path.parent
+        cache_path = dir_path / "cached.dir"
+        is_root = dir_path.samefile(root)
+        if cache_path.exists() and cache_path.is_file():
+            return cls(
+                dir_path,
+                parent=cls.from_dir(dpp, root) if not is_root else None,
+                cache_path=cache_path,
+            )
+        else:
+            return cls(dir_path, parent=cls.from_dir(dpp, root) if not is_root else None)
+
+    def dump(self):
+        for pre, fill, node in RenderTree(self):
+            rprint(f"[yellow]{pre}[/]{node.name}")
+
+    @property
+    def name(self) -> str:
+        return str(self.fspath.name)
+
+
+def get_cached_dirs(root: Path) -> list[PTree]:
     if not root.is_dir():
         raise TypeError(f"Root path '{root}' is not a directory")
+    root = root.absolute()
     cached_files: list[Path] = list(root.walkfiles(is_cached_file))  # type: ignore
     print("cached_files:")
     pprint(cached_files)
-    return [CachedDir(cf) for cf in cached_files]
+    return [PTree.from_cache_file(cf, root) for cf in cached_files]
 
 
 def real_main(args: argparse.Namespace) -> None:
-    dirpath: Path = args.directory
-    cached_dirs = get_cached_dirs(dirpath)
+    dir_path: Path = args.directory
+    cached_dirs = get_cached_dirs(dir_path)
     print("cached_dirs:")
     pprint(cached_dirs)
+    for cd in cached_dirs:
+        cd.dump()
 
 
 def get_arg_parser() -> argparse.ArgumentParser:

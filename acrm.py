f47ed976ba85f7a93c6153b3931eb50a28269b85
Jevin Sweval <jevinsweval@gmail.com>
2025-08-03 15:55:35 -0400

    stupidly simplified

--- acrm.py
+++ acrm.py
@@ -2,105 +2,27 @@
 from __future__ import annotations
 
 import argparse
-from typing import Self
 
-from anytree import NodeMixin, RenderTree
-from attrs import define, field
 from path import Path
-from rich import print as rprint
-from rich.pretty import pprint
-from rich.traceback import install as rtinstall
-
-rtinstall(show_locals=True)
 
 
-def is_cached_file(p: Path) -> bool:
-    if not p.is_file():
-        return False
-    return p.name == "config.cache"
-
-
-@define(init=False, slots=False)
-class PTree(NodeMixin):
-    fspath: Path = field()
-    cache_path: Path | None = field(default=None)
-
-    def __init__(
-        self, path: Path, root: Path, parent: PTree | None, cache_path: Path | None = None
-    ) -> None:
-        super().__init__()
-        if cache_path is not None:
-            if not cache_path.is_file():
-                raise TypeError(f"CachedDir cache_path '{cache_path}' is not a file")
-        self.fspath = root.relpathto(path)
-        self.parent = parent
-        self.cache_path = cache_path
-
-    @property
-    def cached(self) -> bool:
-        return self.cache_path is not None
-
-    @property
-    def parts(self):
-        return self.fspath.parts()
-
-    @classmethod
-    def from_cache_file(cls, cache_file: Path, root: Path) -> Self:
-        if not cache_file.is_file():
-            raise TypeError(f"CachedDir cache_file '{cache_file}' is not a file")
-        dir_path = cache_file.parent
-        dpp = dir_path.parent
-        is_root = dir_path.samefile(root)
-        return cls(
-            dir_path,
-            root,
-            parent=cls.from_dir(dpp, root) if not is_root else None,
-            cache_path=cache_file,
-        )
-
-    @classmethod
-    def from_dir(cls, dir_path: Path, root: Path) -> Self:
-        if not dir_path.is_dir():
-            raise TypeError(f"CachedDir dir_path '{dir_path}' is not a directory")
-        dpp = dir_path.parent
-        cache_path = dir_path / "cached.dir"
-        is_root = dir_path.samefile(root)
-        if cache_path.exists() and cache_path.is_file():
-            return cls(
-                dir_path,
-                root,
-                parent=cls.from_dir(dpp, root) if not is_root else None,
-                cache_path=cache_path,
-            )
-        else:
-            return cls(dir_path, root, parent=cls.from_dir(dpp, root) if not is_root else None)
-
-    def dump(self):
-        for pre, fill, node in RenderTree(self):
-            rprint(f"[yellow]{pre}[/]{node.name}")
-
-    @property
-    def name(self) -> str:
-        return str(self.fspath.name)
-
-
-def get_cached_dirs(root: Path) -> list[PTree]:
+def clean_preserving_cache(root: Path, cache_file: str) -> None:
     if not root.is_dir():
         raise TypeError(f"Root path '{root}' is not a directory")
-    root = root.absolute()
-    cached_files: list[Path] = list(root.walkfiles(is_cached_file))  # type: ignore
-    print("cached_files:")
-    pprint(cached_files)
-    return [PTree.from_cache_file(cf, root) for cf in cached_files]
+    for f in root.walk():
+        if f.is_dir():
+            continue
+        if f.name != cache_file:
+            f.remove()
+    for d in root.walkdirs():
+        pcf = d / cache_file
+        if not pcf.exists():
+            d.rmtree()
 
 
 def real_main(args: argparse.Namespace) -> None:
     dir_path: Path = args.directory
-    cached_dirs = get_cached_dirs(dir_path)
-    print("cached_dirs:")
-    pprint(cached_dirs)
-    for cd in cached_dirs:
-        cd.dump()
+    clean_preserving_cache(dir_path, args.cache_file)
 
 
 def get_arg_parser() -> argparse.ArgumentParser:
@@ -110,6 +32,7 @@ def get_arg_parser() -> argparse.ArgumentParser:
     parser.add_argument(
         "directory", type=Path, help="Directory to [[WARNING]] RECURSIVELY DELETE [[[WARNING]]]"
     )
+    parser.add_argument("-C", "--cache-file", default="config.cache", help="config cache name")
     return parser
 
 
